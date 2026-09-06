0e271470c67057431ab73d50306c6f734b0d0aeb
Brandon Dutton <bdutties@gmail.com>
2026-09-05 20:59:05 -0500

    Added sun to the exclude config

--- config/what_do_they_have.py
+++ config/what_do_they_have.py
@@ -1,5 +1,5 @@
 WHAT_DO_THEY_HAVE_CONFIG = {
     # Vocabulary
     # Keep words requiring "an" or plural sentence logic out of this lesson for now.
-    "excluded_vocabulary": ["Owl", "Corn", "Milk"],
+    "excluded_vocabulary": ["Owl", "Corn", "Milk", "Sun"],
 }
